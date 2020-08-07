92a0a2f2e7f7584e78fbec9530c6aab26a165ef1
Alec Jasik <alec_jasik@yahoo.com>
2020-08-07 19:35:45 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -57,11 +57,5 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-
-
-
-
-
-   # After your code, the following line will return the altered hash
    epic_tragedy
 end
